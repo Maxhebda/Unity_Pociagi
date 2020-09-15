b80b32e695fb3b00f5cd8bc3887339769161f6ce
Maksymilian Hebda <maksymilian.hebda@gmail.com>
2020-09-15 14:28:04 +0200

    add human02

--- Assets/Humans/HumanRandom.cs
+++ Assets/Humans/HumanRandom.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class HumanRandom : MonoBehaviour
 {
     public GameObject human01;
+    public GameObject human02;
 
     public Transform startLeftGlobal;
     public Transform startRightGlobal;
@@ -14,7 +15,7 @@ public class HumanRandom : MonoBehaviour
     float counter = 0;
     void Start()
     {
-        counter = Random.Range(5, 10);
+        counter = Random.Range(2, 10);
     }
 
     void Update()
@@ -23,18 +24,33 @@ public class HumanRandom : MonoBehaviour
         counter-=Time.deltaTime;
         if (counter < 0)
         {
-            counter = Random.Range(5, 10);
-            addHuman01();
+            counter = Random.Range(0, 13);
+            switch(Random.Range((int)0,2))
+            {
+                case 0:
+                    addHuman01();
+                    break;
+                case 1:
+                    addHuman02();
+                    break;
+            }
         }
     }
     private void addHuman01()
     {
         GameObject obj = Instantiate(human01, gameObject.transform);
-        obj.transform.parent = gameObject.transform;
         obj.GetComponent<human01>().startLeft = startLeftGlobal;
         obj.GetComponent<human01>().startRight = startRightGlobal;
         obj.GetComponent<human01>().startUp1 = startUp1Global;
         obj.GetComponent<human01>().startUp2 = startUp2Global;
+    }
+    private void addHuman02()
+    {
+        GameObject obj = Instantiate(human02, gameObject.transform);
+        obj.GetComponent<human02>().startLeft = startLeftGlobal;
+        obj.GetComponent<human02>().startRight = startRightGlobal;
+        obj.GetComponent<human02>().startUp1 = startUp1Global;
+        obj.GetComponent<human02>().startUp2 = startUp2Global;
     }
 
 }
---
+++ Assets/Humans/human02/human02.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class human02 : MonoBehaviour
+{
+    public Transform startLeft;
+    public Transform startRight;
+    public Transform startUp1;
+    public Transform startUp2;
+
+    bool direction;
+    Vector3 nextPosition;
+    float step;
+    float speed = 1f;
+    bool onRotate = false;
+    SpriteRenderer spriteRenderer;
+    void Start()
+    {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        speed = Random.Range(0.3f, 0.7f);
+        direction = Random.Range((int)0, 2) == 0 ? false : true;  // false = left -> right
+        if (direction)
+        {
+            transform.position = startLeft.position;
+            nextPosition = startRight.position;
+            spriteRenderer.flipX = true;
+        }
+        else
+        {
+            transform.position = startRight.position;
+            nextPosition = startLeft.position;
+            spriteRenderer.flipX = false;
+        }
+    }
+
+    void Update()
+    {
+        step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, nextPosition, step);
+        if (transform.position == nextPosition)
+        {
+            gameObject.GetComponent<AudioSource>().Stop();
+            Destroy(gameObject);
+        }
+        if (onRotate)
+        {
+            if (direction)
+            {
+                spriteRenderer.transform.Rotate(Vector3.back * 2);
+            }
+            else
+            {
+                spriteRenderer.transform.Rotate(Vector3.forward * 2);
+            }
+        }
+    }
+    private void OnMouseDown()
+    {
+        if (!direction)
+        {
+            nextPosition = new Vector3(Random.Range(startUp1.position.x, transform.position.x), startUp1.position.y, nextPosition.z);
+        }
+        else
+        {
+            nextPosition = new Vector3(Random.Range(transform.position.x, startUp2.position.x), startUp1.position.y, nextPosition.z);
+        }
+        speed *= 3;
+        onRotate = true;
+        gameObject.GetComponent<AudioSource>().Play();
+    }
+}
